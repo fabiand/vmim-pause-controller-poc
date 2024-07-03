8d537d1b53d0e2b6877b917befed8cfb24b37cd5
Fabian Deutsch <fabiand@fedoraproject.org>
2024-07-03 12:58:14 +0200

    More
    Signed-off-by: Fabian Deutsch <fabiand@fedoraproject.org>

--- Containerfile
+++ Containerfile
@@ -3,5 +3,5 @@ WORKDIR /app
 
 RUN  curl -L "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl" -o /usr/local/bin/kubectl && chmod a+x /usr/local/bin/kubectl
 
-ADD app /
+ADD app/* /app/
 ENTRYPOINT /app/main
--- to.sh
+++ to.sh
@@ -1,5 +1,6 @@
-IMG_REPO=quay.io/fdeutsch/vmim-pause-controller-poc
-NS=vmim-pause-controller-poc
+APP=$(basename $PWD)
+IMG_REPO=quay.io/fdeutsch/$APP
+NS=$APP
 SA=$NS
 
 build() {
@@ -16,25 +17,12 @@ die() { echo "err: $@" ; exit 1; }
 _oc() { echo "$ oc $@" ; oc $@ ; }
 qoc() { oc $@ > /dev/null 2>&1; }
 
-apply() {
-  _oc apply -f manifests/app.yaml
-}
-
-
 deploy() {
-  qoc get project $NS || _oc adm new-project $NS
-  _oc project $NS
-  qoc get sa -n $NS $SA || {
-    _oc create sa -n $NS $SA
-    _oc adm policy add-cluster-role-to-user cluster-admin -z $SA
-    _oc adm policy add-scc-to-user -n $NS privileged -z $SA
-  }
-  apply
+  _oc apply -f manifests/app.yaml
 }
 
 destroy() {
   _oc delete -f manifests/app.yaml
-  _oc delete sa -n $NS $SA
 }
 
 usage() {
